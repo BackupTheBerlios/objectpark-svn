ec86094a48e22712a4c7e8ecc30933f5b6a4470d
katerbau <katerbau@3f4deba8-8cf7-0310-b9c8-e6524590a564>
2005-11-16 23:06:34 +0000

    added missing files

---
+++ Ginko/trunk/GIPhraseBrowserWindow.m
@@ -0,0 +1,58 @@
+//
+//  GIPhraseBrowserWindow.m
+//  GinkoVoyager
+//
+//  Created by Axel Katerbau on 16.11.05.
+//  Copyright 2005 __MyCompanyName__. All rights reserved.
+//
+
+#import "GIPhraseBrowserWindow.h"
+#import "GIPhraseBrowserController.h"
+
+@implementation GIPhraseBrowserWindow
+
+- (void)sendEvent:(NSEvent *)theEvent
+{
+    if ([theEvent type] == NSKeyDown)
+    {
+        switch ([[theEvent characters] characterAtIndex:0])
+        {
+            case '0':
+                [[self delegate] hotkeyPressed:0];
+                return;
+            case '1':
+                [[self delegate] hotkeyPressed:1];
+                return;
+            case '2':
+                [[self delegate] hotkeyPressed:2];
+                return;
+            case '3':
+                [[self delegate] hotkeyPressed:3];
+                return;
+            case '4':
+                [[self delegate] hotkeyPressed:4];
+                return;
+            case '5':
+                [[self delegate] hotkeyPressed:5];
+                return;
+            case '6':
+                [[self delegate] hotkeyPressed:6];
+                return;
+            case '7':
+                [[self delegate] hotkeyPressed:7];
+                return;
+            case '8':
+                [[self delegate] hotkeyPressed:8];
+                return;
+            case '9':
+                [[self delegate] hotkeyPressed:9];
+                return;
+            default:
+                break;
+        }        
+    }
+    
+    [super sendEvent:theEvent];
+}
+
+@end
