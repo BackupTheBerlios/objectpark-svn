fd3858bba802631fb8f5110561100faaa220dba8
westheide <westheide@3f4deba8-8cf7-0310-b9c8-e6524590a564>
2006-01-07 18:48:03 +0000

    - removed unnecessary imports - removed unnecessary declaration of global variables which were needed by the old OPDebugLog implementation

--- Ginko/trunk/main.m
+++ Ginko/trunk/main.m
@@ -8,13 +8,8 @@
 
 
 #import <AppKit/AppKit.h>
-#import "Model/GIMessage.h"
-#import "Model/GIThread.h"
-//#include <stdlib.h>
 #import <SenTestingKit/SenTestProbe.h>
 
- char*  op_debug_setting;
- char** op_debug_domain;
 
 int main(int argc, const char *argv[])
 {
@@ -24,8 +19,9 @@ int main(int argc, const char *argv[])
     //setenv("LD_LIBRARY_PATH", [[[NSBundle mainBundle] privateFrameworksPath] cString], 1);
     
     //[pool release];
-	// Hack to enable desktop launching:
-	if (argc>1 && [[[NSString alloc] initWithCString: argv[1]] hasPrefix: @"-psn"]) argc = 1;
+    // Hack to enable desktop launching:
+	if (argc>1 && [[[NSString alloc] initWithCString: argv[1]] hasPrefix: @"-psn"])
+        argc = 1;
 	
 	return argc>1 ? SenSelfTestMain() : NSApplicationMain(argc, argv);
 }
