d0b997b657c8b586a73c76249f346e83703e0cf6
katerbau <katerbau@3f4deba8-8cf7-0310-b9c8-e6524590a564>
2006-10-04 09:23:41 +0000

    - tweaks to ui experiment

--- Ginko/trunk/Controller/GIThreadListController+ThreadInfoPanel.m
+++ Ginko/trunk/Controller/GIThreadListController+ThreadInfoPanel.m
@@ -15,14 +15,33 @@
 {
 	[self setKeys:[NSArray arrayWithObjects:@"selectedThread", nil]
     triggerChangeNotificationsForDependentKey:@"threadParticipants"];
+	
 	[self setKeys:[NSArray arrayWithObjects:@"selectedThread", nil]
     triggerChangeNotificationsForDependentKey:@"threadMessages"];
+
+	[self setKeys:[NSArray arrayWithObjects:@"selectedThread", nil]
+    triggerChangeNotificationsForDependentKey:@"threadMessagesTitle"];
+
+	[self setKeys:[NSArray arrayWithObjects:@"selectedThread", nil]
+    triggerChangeNotificationsForDependentKey:@"threadParticipantsTitle"];
+
+	[self setKeys:[NSArray arrayWithObjects:@"selectedThread", nil]
+    triggerChangeNotificationsForDependentKey:@"threadTimeSpanDescription"];
 }
 
 - (NSArray *)threadMessages
 {
 	GIThread *thread = [self selectedThread];
 	return [thread valueForKey:@"messages"];
+}
+
+- (NSString *)threadMessagesTitle
+{
+	int count = [[self threadMessages] count];
+	
+	NSString *format = count == 1 ? NSLocalizedString(@"%d Message:" , @"thread info 'thread messages' title") : NSLocalizedString(@"%d Messages:" , @"thread info 'thread messages' title");
+	
+	return [NSString stringWithFormat:format, count];
 }
 
 - (NSArray *)threadParticipants
@@ -58,6 +77,15 @@
 	return result;
 }
 
+- (NSString *)threadParticipantsTitle
+{
+	int count = [[self threadParticipants] count];
+	
+	NSString *format = count == 1 ? NSLocalizedString(@"%d Participant:" , @"thread info 'thread participants' title") : NSLocalizedString(@"%d Participants:" , @"thread info 'thread participants' title");
+	
+	return [NSString stringWithFormat:format, count];
+}
+
 - (int)infoPanelTableViewFontSize
 {
 	return 10;
@@ -66,6 +94,25 @@
 - (int)infoPanelTableViewRowHeight
 {
 	return 12;
+}
+
+- (NSString *)threadTimeSpanDescription
+{
+	NSArray *messages = [self threadMessages];
+	
+	if ([messages count] == 0) return @"";
+	
+	GIMessage *firstMessage = [messages objectAtIndex:0];
+	GIMessage *lastMessage = [messages lastObject];
+	
+	if (firstMessage == lastMessage)
+	{
+		return [infoPanelDateFormatter stringForObjectValue:[firstMessage valueForKey:@"date"]];
+	}
+	else
+	{
+		return [NSString stringWithFormat:@"%@ - %@", [infoPanelDateFormatter stringForObjectValue:[firstMessage valueForKey:@"date"]], [infoPanelDateFormatter stringForObjectValue:[lastMessage valueForKey:@"date"]]];
+	}
 }
 
 @end
