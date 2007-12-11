05e8e04553ce41e095f676dd3ddc864d82d284fc
katerbau <katerbau@3f4deba8-8cf7-0310-b9c8-e6524590a564>
2007-12-11 10:02:54 +0000

    added preferences pane and general preferences

---
+++ Gina/trunk/GIUserDefaultsKeys.h
@@ -0,0 +1,58 @@
+/*
+ *  UserDefaultsKeys.h
+ *  GinkoVoyager
+ *
+ *  Created by Dirk Theisen on 22.12.04.
+ *  Copyright 2004 Objectpark Group <http://www.objectpark.org>. All rights reserved.
+ *
+ */
+
+enum GISetSeenBehavior
+{
+	GISetSeenBehaviorImmediately = 0,
+	GISetSeenBehaviorAfterTimeinterval, // needs time interval from e.g. user defaults (default should be 1.5 seconds)
+	GISetSeenBehaviorManually
+};
+
+extern NSString *OpenMessageGroups;
+extern NSString *MessageRendererFontName;
+extern NSString *MessageRendererFontSize;
+extern NSString *MessageRendererShouldRenderAttachmentsInlineIfPossible;
+extern NSString *UseAlternateDateDisplayInMessageLists;
+extern NSString *ShowAllHeaders;
+extern NSString *ShowRawSource;
+extern NSString *ContentTypePreferences;
+extern NSString *GroupsDrawerMode;
+extern NSString *HeadersShown;
+extern NSString *AdditionalHeadersShown;
+extern NSString *SentMessageGroupURLString;
+extern NSString *DefaultMessageGroupURLString;
+extern NSString *DraftsMessageGroupURLString;
+extern NSString *QueuedMessageGroupURLString;
+extern NSString *SpamMessageGroupURLString;
+extern NSString *TrashMessageGroupURLString;
+extern NSString *DisableKeychainForPasswortDefault;
+extern NSString *ImportPanelLastDirectory;
+extern NSString *AutomaticActivityPanelEnabled;
+extern NSString *FulltextIndexChangeCount;
+extern NSString *SearchHitLimit;
+extern NSString *JunkReplySubjectPrefixes;
+extern NSString *EarliestSendTimes;
+extern NSString *AskAgainToBecomeDefaultMailApplication;
+extern NSString *SoonRipeMessagesShouldBeSent;
+extern NSString *SoonRipeMessageMinutes;
+extern NSString *DefaultProfileURLString;
+extern NSString *ContinuousSpellCheckingEnabled;
+extern NSString *RecentThreadListWindowPositions;
+extern NSString *AttachmentSaveFolder;
+extern NSString *ReuseThreadListWindowByDefault;
+extern NSString *DateOfLastMessageRetrieval;
+extern NSString *ShowThreadInfoPanel;
+extern NSString *SelectFirstUnreadMessageInThread;
+extern NSString *SetSeenBehavior;
+extern NSString *SetSeenTimeinterval;
+
+extern NSArray *allAdditionalHeadersForDisplay();
+extern void registerDefaultDefaults();
+
+#define DEFAULTSEARCHHITLIMIT 200
---
+++ Gina/trunk/GIUserDefaultsKeys.m
@@ -0,0 +1,123 @@
+/*
+ *  UserDefaultsKeys.c
+ *  GinkoVoyager
+ *
+ *  Created by Dirk Theisen on 22.12.04.
+ *  Copyright 2004 Objectpark Group <http://www.objectpark.org>. All rights reserved.
+ *
+ */
+
+#import "GIUserDefaultsKeys.h"
+
+NSString *OpenMessageGroups = @"OpenMessageGroups";
+NSString *MessageRendererFontName = @"MessageRendererFontName";
+NSString *MessageRendererFontSize = @"MessageRendererFontSize";
+NSString *MessageRendererShouldRenderAttachmentsInlineIfPossible = @"MessageRendererShouldRenderAttachmentsInlineIfPossible";
+NSString *UseAlternateDateDisplayInMessageLists = @"UseAlternateDateDisplayInMessageLists";
+NSString *ShowAllHeaders = @"ShowAllHeaders";
+NSString *ShowRawSource = @"ShowRawSource";
+NSString *ContentTypePreferences = @"ContentTypePreferences";
+NSString *GroupsDrawerMode = @"GroupsDrawerMode";
+NSString *HeadersShown = @"GIHeadersShown";
+NSString *AdditionalHeadersShown  = @"GIAdditionalHeadersShown";
+NSString *SentMessageGroupURLString = @"SentMessageGroupURLString";
+NSString *DefaultMessageGroupURLString = @"DefaultMessageGroupURLString";
+NSString *DraftsMessageGroupURLString = @"DraftsMessageGroupURLString";
+NSString *QueuedMessageGroupURLString = @"QueuedMessageGroupURLString";
+NSString *SpamMessageGroupURLString = @"SpamMessageGroupURLString";
+NSString *TrashMessageGroupURLString = @"TrashMessageGroupURLString";
+NSString *ImportPanelLastDirectory = @"ImportPanelLastDirectory";
+NSString *AutomaticActivityPanelEnabled = @"AutomaticActivityPanelEnabled";
+NSString *FulltextIndexChangeCount = @"FulltextIndexChangeCount";
+NSString *EarliestSendTimes = @"EarliestSendTimes";
+NSString *AskAgainToBecomeDefaultMailApplication = @"AskAgainToBecomeDefaultMailApplication";
+NSString *SoonRipeMessagesShouldBeSent = @"SoonRipeMessagesShouldBeSent";
+NSString *SoonRipeMessageMinutes = @"SoonRipeMessageMinutes";
+NSString *DefaultProfileURLString = @"DefaultProfileURLString";
+NSString *ContinuousSpellCheckingEnabled = @"ContinuousSpellCheckingEnabled";
+
+NSString *DisableKeychainForPasswortDefault = @"DisableKeychainForPasswortDefault";
+
+NSString *SearchHitLimit = @"SearchHitLimit";
+NSString *JunkReplySubjectPrefixes = @"JunkReplySubjectPrefixes";
+NSString *RecentThreadListWindowPositions = @"RecentThreadListWindowPositions";
+NSString *AttachmentSaveFolder = @"AttachmentSaveFolder";
+NSString *ReuseThreadListWindowByDefault = @"ReuseThreadListWindowByDefault";
+NSString *DateOfLastMessageRetrieval = @"DateOfLastMessageRetrieval";
+NSString *ShowThreadInfoPanel = @"ShowThreadInfoPanel";
+NSString *SelectFirstUnreadMessageInThread = @"SelectFirstUnreadMessageInThread";
+NSString *SetSeenBehavior = @"SetSeenBehavior";
+NSString *SetSeenTimeinterval = @"SetSeenTimeinterval";
+
+NSArray *allAdditionalHeadersForDisplay()
+{
+    return [NSArray arrayWithObjects:
+			@"Organization",
+			@"X-Newsreader", 
+			@"X-Mailer", 
+			@"User-Agent",
+			@"X-Complaints-To",
+			@"Content-Type", 
+			@"Content-Transfer-Encoding",
+			@"Lines",
+			@"Xref",
+			@"Path",
+			@"Delivered-To",
+			@"Precedence",
+			@"List-Id",
+			@"List-Archive",
+			@"List-Help",
+			@"X-Profile",
+			@"X-Sender",
+			@"X-Accept-Language",
+			nil];
+}
+
+void registerDefaultDefaults()
+{    
+    NSValueTransformer *archiverTransformer = [NSValueTransformer valueTransformerForName:NSUnarchiveFromDataTransformerName];
+    
+    NSDictionary *appDefaults = [NSDictionary dictionaryWithObjectsAndKeys:
+								 [NSArray arrayWithObjects:
+								  @"From",
+								  @"Newsgroups",
+								  @"Subject",
+								  @"To",
+								  @"Cc",
+								  @"Bcc",
+								  @"Reply-To",
+								  @"Date",
+								  nil], HeadersShown,
+								 
+								 [NSArray arrayWithObjects:@"Antw: ", nil], JunkReplySubjectPrefixes,
+								 
+								 [NSArray array], AdditionalHeadersShown,
+								 
+								 [NSArray arrayWithObjects:
+								  @"multipart/mixed",
+								  @"text/enriched",
+								  @"text/plain",
+								  @"text/html",
+								  nil], ContentTypePreferences,
+								 
+								 // setting default sort ordering for phrases list in phrase browser:
+								 [archiverTransformer reverseTransformedValue:[NSArray arrayWithObject:[[[NSSortDescriptor alloc] initWithKey:@"phrasename" ascending:YES] autorelease]]], @"phrasesortdescriptors",
+								 
+								 [NSNumber numberWithInt:DEFAULTSEARCHHITLIMIT], SearchHitLimit,
+								 
+								 [NSNumber numberWithBool:YES], AskAgainToBecomeDefaultMailApplication, 
+								 
+								 [NSNumber numberWithBool:YES], SoonRipeMessagesShouldBeSent,
+								 
+								 [NSNumber numberWithInt:30], SoonRipeMessageMinutes,
+								 
+								 [NSNumber numberWithBool:YES], NSPrintHeaderAndFooter,
+								 
+								 [NSNumber numberWithBool:YES], SelectFirstUnreadMessageInThread,
+								 
+								 [NSNumber numberWithFloat:1.5], SetSeenTimeinterval,
+								 
+								 nil, nil];
+    
+    [[NSUserDefaults standardUserDefaults] registerDefaults: appDefaults];
+}
