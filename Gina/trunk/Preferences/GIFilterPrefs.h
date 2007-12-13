6bd607c0d65143fb8c689f06eb0fde2e9fbda3cf
katerbau <katerbau@3f4deba8-8cf7-0310-b9c8-e6524590a564>
2007-12-13 09:54:32 +0000

    added emtpy filter/sorting preference pane

---
+++ Gina/trunk/Preferences/GIFilterPrefs.h
@@ -0,0 +1,17 @@
+//
+//  GIFilterPrefs.h
+//  Gina
+//
+//  Created by Axel Katerbau on 13.12.07.
+//  Copyright 2007 Objectpark Group. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <OPPreferences/OPPreferences.h>
+
+@interface GIFilterPrefs : OPPreferencePane 
+{
+
+}
+
+@end
---
+++ Gina/trunk/Preferences/GIFilterPrefs.m
@@ -0,0 +1,14 @@
+//
+//  GIFilterPrefs.m
+//  Gina
+//
+//  Created by Axel Katerbau on 13.12.07.
+//  Copyright 2007 Objectpark Group. All rights reserved.
+//
+
+#import "GIFilterPrefs.h"
+
+
+@implementation GIFilterPrefs
+
+@end
