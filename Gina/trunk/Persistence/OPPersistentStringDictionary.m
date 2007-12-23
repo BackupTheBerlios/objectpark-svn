56d3172926305836a7c8b7db320de208a74df34d
theisen <theisen@3f4deba8-8cf7-0310-b9c8-e6524590a564>
2007-12-23 14:47:13 +0000

    Preparing for test.

--- Gina/trunk/Persistence/OPPersistentStringDictionary.m
+++ Gina/trunk/Persistence/OPPersistentStringDictionary.m
@@ -28,6 +28,52 @@
 	return [OPPersistentObjectContext defaultContext];
 }
 
+- (OID) oid
+/*" Returns the object id for the receiver or NILOID if the object has no context.
+ Currently, the defaultContext is always used. "*/
+{
+	if (!oid) {
+		@synchronized(self) {
+			// Create oid on demand, this means that this is now becoming persistent.
+			// Persistent objects unarchived are explicitly given an oid:
+			
+			OPPersistentObjectContext* context = [self context];
+			if (context) {
+				[context insertObject: self];
+			}
+		}
+	}
+	return oid;
+}
+
+- (OID) currentOID
+/*" Private method to be used within the framework only. "*/
+{
+	return oid;
+}
+
+- (void) setOID: (OID) theOid
+/*" Registers the receiver with the context, if neccessary. "*/
+{
+	@synchronized(self) {
+		if (oid != theOid) {
+			NSAssert(oid==0, @"Object ids can be set only once per instance.");
+			oid = theOid;
+			[[self context] registerObject: self];
+		}
+	}
+}
+
++ (BOOL) cachesAllObjects
+{
+	return NO;
+}
+
++ (BOOL) canPersist
+{
+	return YES;
+}
+
 - (Class) classForCoder
 {
 	return [self class];
--- Gina/trunk/Persistence/Tests/OPPersistentTestObject.m
+++ Gina/trunk/Persistence/Tests/OPPersistentTestObject.m
@@ -21,6 +21,7 @@
 	return self;
 }
 
+
 - (void) dealloc
 {
 	[name release];
