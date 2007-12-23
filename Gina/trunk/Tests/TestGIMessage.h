b16bd0ca17b74ebec0db8613063aa87fa41ba4c2
katerbau <katerbau@3f4deba8-8cf7-0310-b9c8-e6524590a564>
2007-12-23 14:29:14 +0000

    fixed test

--- Gina/trunk/Tests/TestGIMessage.h
+++ Gina/trunk/Tests/TestGIMessage.h
@@ -3,7 +3,7 @@
 //  Gina
 //
 //  Created by Axel Katerbau on 22.12.07.
-//  Copyright 2007 __MyCompanyName__. All rights reserved.
+//  Copyright 2007 Objectpark Group. All rights reserved.
 //
 
 #import <SenTestingKit/SenTestingKit.h>
@@ -11,7 +11,6 @@
 
 @interface TestGIMessage : SenTestCase 
 {
-
 }
 
 @end
