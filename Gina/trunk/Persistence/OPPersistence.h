afa0f891a2df5fba180895a98a38ee0c48e31605
katerbau <katerbau@3f4deba8-8cf7-0310-b9c8-e6524590a564>
2007-12-24 09:10:46 +0000

    removed schlunz

---
+++ Gina/trunk/Persistence/OPPersistence.h
@@ -0,0 +1,18 @@
+//
+//  OPPersistence.h
+//  BTreeLite
+//
+//  Created by Axel Katerbau on 24.12.07.
+//  Copyright 2007 Axel Katerbau. All rights reserved.
+//
+
+#import "OPDBLite.h"
+#import "OPFaultingArray.h"
+#import "OPKeyedArchiver.h"
+#import "OPKeyedUnarchiver.h"
+#import "OPPersistenceConstants.h"
+#import "OPPersistenceWrapper.h"
+#import "OPPersistentObject.h"
+#import "OPPersistentObjectContext.h"
+#import "OPPersistentSet.h"
+#import "OPPersistentStringDictionary.h"
