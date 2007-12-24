223fec2fb1e684bacea566fc17ce09df74be3c99
katerbau <katerbau@3f4deba8-8cf7-0310-b9c8-e6524590a564>
2007-12-24 09:30:42 +0000

    fixed class hierarchy of tests

---
+++ Gina/trunk/Tests/TestGIMessageGroup.h
@@ -0,0 +1,17 @@
+//
+//  TestGIMessageGroup.h
+//  Gina
+//
+//  Created by Axel Katerbau on 24.12.07.
+//  Copyright 2007 Objectpark Group. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+#import "TestWithPersistence.h"
+
+@interface TestGIMessageGroup : TestWithPersistence 
+{
+
+}
+
+@end
---
+++ Gina/trunk/Tests/TestGIMessageGroup.m
@@ -0,0 +1,19 @@
+//
+//  TestGIMessageGroup.m
+//  Gina
+//
+//  Created by Axel Katerbau on 24.12.07.
+//  Copyright 2007 Objectpark Group. All rights reserved.
+//
+
+#import "TestGIMessageGroup.h"
+#import "GIMessageGroup.h"
+
+@implementation TestGIMessageGroup
+
+- (void)testGroupThreadRelationship
+{
+	GIMessageGroup *group = [GIMessageGroup defaultMessageGroup];
+}
+
+@end
