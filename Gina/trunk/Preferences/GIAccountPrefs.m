df974ed453b71fe70f00132307e954a3dad785e3
katerbau <katerbau@3f4deba8-8cf7-0310-b9c8-e6524590a564>
2007-12-27 13:07:37 +0000

    added sort descriptor for account prefs

--- Gina/trunk/Preferences/GIAccountPrefs.m
+++ Gina/trunk/Preferences/GIAccountPrefs.m
@@ -21,31 +21,49 @@
 - (IBAction)removeAccount:(id)sender
 {	
 	NSArray *selectedObjects = [accountsController selectedObjects];
-//	int count = [selectedObjects count];
+	int count = [selectedObjects count];
 	GIAccount *selectedAccount = [selectedObjects lastObject];
-//	int selectedRow = [accountTableView selectedRow];
+	int selectedRow = [accountTableView selectedRow];
 
 	[self willChangeValueForKey:@"accounts"];
+#warning Axel->Dirk: How to get rid of the account?
 	[selectedAccount delete];
+	[selectedAccount release];
 	[[selectedAccount context] saveChanges];
 	[self didChangeValueForKey:@"accounts"];
     
-//	[accountTableView selectRow:MIN(selectedRow, count - 2) byExtendingSelection:NO];
+	[accountTableView selectRow:MIN(selectedRow, count - 2) byExtendingSelection:NO];
 }
 
 - (IBAction)addAccount:(id)sender
 {
 	OPPersistentObjectContext *context = [OPPersistentObjectContext defaultContext];
+	
 	[self willChangeValueForKey:@"accounts"];
-	[context insertObject:[[GIAccount alloc] init]];
+	GIAccount *newAccount = [[GIAccount alloc] init];
+	[context insertObject:newAccount];
 	[context saveChanges];
 	[self didChangeValueForKey:@"accounts"];
+	
+	[accountsController setSelectedObjects:[NSArray arrayWithObject:newAccount]];
 }
 
 - (IBAction)rearrangeObjects:(id)sender;
 /*" This action triggers the refresh of interface elements that change their value according to the actual change. "*/
 {
     [accountsController rearrangeObjects];
+}
+
+- (NSArray *)accountSortDescriptors
+{
+	static NSArray *accountSortDescriptors = nil;
+	
+	if (!accountSortDescriptors)
+	{
+		accountSortDescriptors = [[NSArray alloc] initWithObjects:[[[NSSortDescriptor alloc] initWithKey:@"name" ascending:YES] autorelease], nil];
+	}
+	
+	return accountSortDescriptors;
 }
 
 @end
